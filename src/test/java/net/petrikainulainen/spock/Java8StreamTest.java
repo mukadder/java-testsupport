ca5145a0b58effc1f5cb180f0d030a7bee2dbd4c
evavanya <hht179@gmail.com>
2016-01-08 15:29:04 -0500

    cxcfgdfgvdf

--- src/test/java/net/petrikainulainen/spock/Java8StreamTest.java
+++ src/test/java/net/petrikainulainen/spock/Java8StreamTest.java
@@ -317,58 +317,7 @@ items = new ArrayList<>();
 		}).collect(Collectors.toList());
     	
     }
-  @Test
-  
-  public void testmacd() throws Exception{
-	  Stream<String> lines = Files.lines(Paths.get("files", "mcdonalds.csv")) ;
-      List<McDonald> mcdos = lines.map(s -> {
-          // -149.95038,61.13712,"McDonalds-Anchorage,AK","3828 W Dimond Blvd, Anchorage,AK, (907) 248-0597"
-          // -72.84817,41.27988,"McDonalds-Branford,CT","424 W Main St, Branford,CT, (203) 488-9353"
-          String [] strings = s.split(",") ;
-          McDonald mdo = new McDonald() ;
-          mdo.setLatitude(Double.parseDouble(strings[0])) ;
-          mdo.setLongitude(Double.parseDouble(strings[1])) ;
-          mdo.setName(strings[2].substring(1) + strings[3].substring(0, strings[3].length() - 1)) ;
-          mdo.setAddress(strings[4].substring(1)) ;
-          mdo.setCity(strings[5].trim()) ;
-          mdo.setState(strings[6].trim()) ;
-          if (mdo.state().endsWith("\"")) {
-              mdo.setState(mdo.state().substring(0, mdo.state().length() - 1)) ;
-          }
-          if (mdo.state().contains(" ")) {
-              mdo.setState(mdo.state().substring(0, mdo.state().indexOf(" "))) ;
-          }
-          if (mdo.state().length() > 2) {
-              mdo.setState(strings[7].trim()) ;
-          }
-          return mdo ;
-      }).collect(Collectors.toList()) ;
-      
-	  assertEquals(13912,mcdos.size());
-	  System.out.println("# of McDos = " + mcdos.size()) ;
-	  // The number of cities that have a McDonald
-      long nTowns = 
-      mcdos.stream()
-              .map(McDonald::city)
-              .collect(Collectors.toSet())
-              .size() ;
-      assertEquals(4927,nTowns);
-      System.out.println("The number of cities that have a McDonald : " + nTowns) ;
-      // The city has the most MacDonald
-      Map.Entry<String, Long> entry = 
-      mcdos.stream()
-              .collect(
-                      Collectors.groupingBy(
-                              McDonald::city, 
-                              Collectors.counting()
-                      )
-              )
-              .entrySet()
-              .stream()
-              .max(Map.Entry.comparingByValue())
-              .get() ;
-      System.out.println("The city has the most MacDonald : " + entry) ;
-  }
+ 
  /* We have a list of Student class. Grouping is done on the basis of student class name.
   List is converted into stream of student object. 
   Then call collect method of stream. groupingBy of Collectors class checks each element of stream 
